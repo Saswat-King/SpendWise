bfc6f7da33d7703790a498a821ee7b52d8a52f94
Saswat -King <lenavogaming123@gmail.com>
2026-07-06 12:04:11 +0530

    Add repository bindings

--- app/src/main/java/com/kingstudio/spendwise/di/RepositoryModule.kt
+++ app/src/main/java/com/kingstudio/spendwise/di/RepositoryModule.kt
@@ -1,11 +1,37 @@
 package com.kingstudio.spendwise.di
 
+import com.kingstudio.spendwise.data.repository.BudgetRepository
+import com.kingstudio.spendwise.data.repository.BudgetRepositoryImpl
+import com.kingstudio.spendwise.data.repository.CategoryRepository
+import com.kingstudio.spendwise.data.repository.CategoryRepositoryImpl
+import com.kingstudio.spendwise.data.repository.ExpenseRepository
+import com.kingstudio.spendwise.data.repository.ExpenseRepositoryImpl
+import com.kingstudio.spendwise.data.repository.IncomeRepository
+import com.kingstudio.spendwise.data.repository.IncomeRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCategoryRepository(impl: CategoryRepositoryImpl): CategoryRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindExpenseRepository(impl: ExpenseRepositoryImpl): ExpenseRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindBudgetRepository(impl: BudgetRepositoryImpl): BudgetRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindIncomeRepository(impl: IncomeRepositoryImpl): IncomeRepository
 }
